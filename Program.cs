64a2afd51b20debf115d8eef7248d2dcf7141339
Hammad Ul Haq <cse.hammad@gmail.com>
2021-11-18 19:01:20 +0500

    v1 Commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", MyHandler.Hello);
+
+
+app.Run();
+
+class MyHandler
+{
+public static string Hello()
+    { return "Hello World!"; }
+}
